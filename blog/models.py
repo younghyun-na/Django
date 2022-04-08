b2f6f58b0d570886c27d114edfab29c05afc7d3c
younghyun-na <younghyunna@naver.com>
2022-04-08 17:38:25 +0900

    feat: post에 카테고리 필드 추가

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,16 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)  # url에 들어갈 수 없는 문자들 지원
+
+    def __str__(self):
+        return self.name
+
+    class Meta:   # override
+        verbose_name_plural : 'Categories'
+
 class Post(models.Model):
     title = models.CharField(max_length=30)
     hook_msg = models.TextField(blank=True)
